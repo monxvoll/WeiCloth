8201bd0a662d0139fb4797ad1f44d08ffee482c7
monxvll <stedy3348112688@gmail.com>
2026-03-20 21:08:34 -0500

    Add Angular modularization with creation of modules and components

---
+++ weicloth/src/app/clothes/inventory-dashboard/inventory-dashboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory-dashboard',
+  imports: [],
+  templateUrl: './inventory-dashboard.html',
+  styleUrl: './inventory-dashboard.css',
+})
+export class InventoryDashboard {}
---
+++ weicloth/src/app/recomendation/item-carousel/item-carousel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-carousel',
+  imports: [],
+  templateUrl: './item-carousel.html',
+  styleUrl: './item-carousel.css',
+})
+export class ItemCarousel {}
